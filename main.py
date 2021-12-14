cd4d95920c7033338196e4256f3bbed0f8b904dc
yuna-liu <89393522+yuna-liu@users.noreply.github.com>
2021-12-14 09:49:28 +0100

    run main.py to generate dashboard

--- main.py
+++ main.py
@@ -1,66 +1,119 @@
-import pandas as pd
 from dash import dcc, html
+from dash.dependencies import Output, Input
 import dash
 from load_data import StockDataLocal
-from dash.dependencies import Output, Input
 import plotly_express as px
 from time_filtering import filter_time
+import pandas as pd
 import dash_bootstrap_components as dbc
 
+# module variables
+
+# create an object of type StockDataLocal so that we can load data
 stock_data_object = StockDataLocal()
 
-symbol_dict = dict(AAPL="Apple", NVDA="Nvidia", TSLA="Tesla", IBM="IBM")
+
+symbol_dict = {"AAPL": "Apple", "NVDA": "Nvidia",
+               "TSLA": "Tesla", "IBM": "IBM"}
+
+# stock_symbol: [daily_df, intradaily_df]
+df_dict = {symbol: stock_data_object.stock_dataframe(
+    symbol) for symbol in symbol_dict}
+
 
 stock_options_dropdown = [{"label": name, "value": symbol}
                           for symbol, name in symbol_dict.items()]
 
-df_dict = {symbol: stock_data_object.stock_dataframe(symbol)
-           for symbol in symbol_dict}
-
-# OHLC options - Open, High, Low, Close
+# ohlc - Open, High, Low, Close
 ohlc_options = [{"label": option.capitalize(), "value": option}
                 for option in ["open", "high", "low", "close"]]
 
 slider_marks = {i: mark for i, mark in enumerate(
-    ["1 day", "1 week", "1 month", "3 months",
-     "1 year", "5 years", "Max"]
-)}
+    ["1 day", "1 week", "1 month", "3 months", "1 year", "5 year", "Max"])}
 
-app = dash.Dash(__name__)
+stylesheets = [dbc.themes.MATERIA]
+# creates a Dash App
+app = dash.Dash(__name__, external_stylesheets=stylesheets,
+                meta_tags=[dict(name="viewport", content="width=device-width, initial-scale=1.0")])
 
-server = app.server
-
-app.layout = html.Div([
-    html.H1("Stocks viewer"),
-    html.P("Choose a stock"),
-    dcc.Dropdown(id='stock-picker-dropdown', className='',
-                 options=stock_options_dropdown,
-                 value='AAPL'
-                 ),
-    html.P(id="highest-value"),
-    html.P(id="lowest-value"),
-    dcc.RadioItems(id='ohlc-radio', className='',
-                   options=ohlc_options,
-                   value='close'
-                   ),
-    dcc.Graph(id='stock-graph', className=''),
-
-    dcc.Slider(id='time-slider', className='',
-               min=0, max=6,
-               step=None,
-               value=2,
-               marks=slider_marks),
-
-    # stores an intermediate value on clients browser for sharing between callbacks
-    dcc.Store(id="filtered-df")
-])
+server = app.server  # needed for Heroku to connect to
 
 
-@app.callback(Output("filtered-df", "data"),
-              Input("stock-picker-dropdown", "value"),
+
+app.layout = dbc.Container([
+    dbc.Card([
+        dbc.CardBody([
+            html.H1('Stocks viewer',
+                    className='card-title text-dark mx-3')
+        ])
+    ], className="mt-4"),
+
+    dbc.Row(className='mt-4', children=[
+        dbc.Col(
+            # responsivity
+            html.P("Choose a stock"), xs="12", sm="12", md="6", lg="4", xl={"size": 1, "offset": 2},
+            className="mt-1"
+        ),
+        dbc.Col(
+            dcc.Dropdown(id='stock-picker-dropdown', className='',
+                         options=stock_options_dropdown,
+                         value='AAPL',
+                         placeholder='Apple'), xs="12", sm="12", md="12", lg="4", xl="3"),
+
+        dbc.Col([
+            dbc.Card([
+                dcc.RadioItems(id='ohlc-radio', className="m-1",
+                                  options=ohlc_options,
+                                  value='close'
+                               ),
+            ])
+        ], xs="12", sm="12", md="12", lg='4', xl="3"),
+    ]),
+
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id="stock-graph"),
+            dcc.Slider(id='time-slider',
+                       min=0,
+                       max=6,
+                       step=None,
+                       value=2,
+                       marks=slider_marks
+                       ),
+        ], lg={"size": "6", "offset": 1}, xl={"size": "6", "offset": 1}),
+
+        dbc.Col([
+            dbc.Row(
+                dbc.Card([
+                    html.H2("Highest value", className="h5 mt-3 mx-3"),
+                    html.P(id="highest-value", className="text-success h1 mx-2")
+                ]), className="mt-5 h-25"
+            ),
+            dbc.Row(
+                dbc.Card([
+                    html.H2("Lowest value", className="h5 mt-3 mx-3"),
+                    html.P(id="lowest-value", className="text-danger h1 mx-2")
+                ]),
+                className="mt-5 h-25"
+            ),
+        ], sm="5", md="3", lg="3", xl="2", className="mt-5 mx-5"),
+
+        html.Footer([
+            html.H3("Stock viewer 2021", className="h6"),
+            html.P("Dashboard av Kokchun Giang")],
+            className="navbar fixed-bottom")
+
+    ]),
+
+    # stores an intermediate value on the clients browser for sharing between callbacks
+    dcc.Store(id="filtered-df"),
+], fluid=True)
+
+
+@app.callback(Output("filtered-df", "data"), Input("stock-picker-dropdown", "value"),
               Input("time-slider", "value"))
 def filter_df(stock, time_index):
-    """Filters the dataframe abd stores in intermediary for callbacks
+    """Filters the dataframe and stores it intermediary for usage in callbacks
     Returns:
         json object of filtered dataframe
     """
@@ -68,25 +121,33 @@ def filter_df(stock, time_index):
 
     dff = dff_intraday if time_index <= 2 else dff_daily
 
-    # maps 0-6 to number of days
     days = {i: day for i, day in enumerate([1, 7, 30, 90, 365, 365*5])}
 
-    dff = dff if time_index == 6 else filter_time(dff, days[time_index])
+    dff = dff if time_index == 6 else filter_time(dff, days=days[time_index])
 
     return dff.to_json()
+
+# when something changes in the input component, the code in function below will run and update the output component
+# the components are connected through their id
+
 
 @app.callback(
     Output("stock-graph", "figure"),
     Input("filtered-df", "data"),
-    Input("stock-picker-dropdown", "value"),
-    Input("ohlc-radio", "value")
+    Input("ohlc-radio", "value"),
+    Input("stock-picker-dropdown", "value")
 )
-def update_graph(json_df, stock, ohlc):
-    
-    dff = pd.read_json(json_df) 
-    fig = px.line(dff, x=dff.index, y=ohlc, title = symbol_dict[stock])
+def update_graph(json_df, ohlc, stock):
+    """Updates graph based on different unputs"""
 
-    return fig  # fig object goes into Output property i.e. figure property
+    dff = pd.read_json(json_df)
+
+    fig = px.line(dff, x=dff.index,
+                  y=ohlc, title=symbol_dict[stock])
+
+    # TODO: Add figure settings to make graph look nice
+
+    return fig
 
 
 @app.callback(
@@ -96,11 +157,9 @@ def update_graph(json_df, stock, ohlc):
     Input("ohlc-radio", "value")
 )
 def highest_lowest_value(json_df, ohlc):
-    
     dff = pd.read_json(json_df)
-    highest_value = f"High {dff[ohlc].max():.1f}"
-    lowest_value = f"Low {dff[ohlc].min():.1f}"
-    
+    highest_value = f"{dff[ohlc].max():.2f} USD"
+    lowest_value = f"{dff[ohlc].min():.2f} USD"
     return highest_value, lowest_value
 
 
